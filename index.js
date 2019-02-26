76150ec2f0633f0f7f5f27550f899b91092cfe46
s.siefke.ext@bitworkers.eu <simon.siefke@gmail.com>
2019-02-26 19:07:07 +0100

    feat: add vsce

--- index.js
+++ index.js
@@ -2,6 +2,28 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
     '@semantic-release/git',
+  ],
+  verifyConditions: [
+    '@semantic-release/changelog',
+    'semantic-release-vsce',
+    '@semantic-release/github',
+  ],
+  prepare: [
+    {
+      path: '@semantic-release/changelog',
+    },
+    {
+      path: 'semantic-release-vsce',
+      packageVsix: 'chatdown.vsix',
+    },
+  ],
+  publish: [
+    'semantic-release-vsce',
+    {
+      path: '@semantic-release/github',
+      assets: 'chatdown.vsix',
+    },
   ],
 }
