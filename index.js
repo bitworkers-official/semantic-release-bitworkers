106045e13dd61e930d80e8a266bf9847a8e21a29
s.siefke.ext@bitworkers.eu <simon.siefke@gmail.com>
2019-02-26 19:21:05 +0100

    fix: missing dep

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 module.exports = {
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
-    '@semantic-release/git',
-  ],
-  verifyConditions: [
-    '@semantic-release/changelog',
-    'semantic-release-vsce',
-    '@semantic-release/github',
-  ],
+  verifyConditions: ['semantic-release-vsce'],
   prepare: [
-    {
-      path: '@semantic-release/changelog',
-    },
     {
       path: 'semantic-release-vsce',
       packageVsix: 'chatdown.vsix',
