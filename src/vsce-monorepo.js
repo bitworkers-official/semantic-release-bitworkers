fab01fade8aa786c2eb35f46f629fee15a1e92bd
s.siefke.ext@bitworkers.eu <simon.siefke@gmail.com>
2019-04-10 18:46:15 +0200

    feat: add vsce monorepo

---
+++ src/vsce-monorepo.js
@@ -0,0 +1,21 @@
+module.exports = {
+  verifyConditions: ['semantic-release-vsce', '@semantic-release/github'],
+  prepare: [
+    {
+      path: '@semantic-release/changelog',
+      changelogFile: '../CHANGELOG.md',
+    },
+    '@semantic-release/git',
+    {
+      path: 'semantic-release-vsce',
+      packageVsix: 'extension.vsix',
+    },
+  ],
+  publish: [
+    'semantic-release-vsce',
+    {
+      path: '@semantic-release/github',
+      assets: 'extension.vsix',
+    },
+  ],
+}
